195610f0f72054876de530ae64902e27c320a613
Paola Segura <145728086+Paobella@users.noreply.github.com>
2025-06-04 16:38:29 -0600

    Actualizacion de Api

---
+++ Taller Industrial Brenes API/Controllers/AutenticacionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Taller_Industrial_Brenes_API.Controllers
+{
+    public class AutenticacionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
