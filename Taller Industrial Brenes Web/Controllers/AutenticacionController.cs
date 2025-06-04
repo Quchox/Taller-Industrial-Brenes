7026732829eb55e7232a4bebfe5592010f8c2a0f
Paola Segura <145728086+Paobella@users.noreply.github.com>
2025-06-04 16:11:54 -0600

    AutenticacionController y usuarioModel

---
+++ Taller Industrial Brenes Web/Controllers/AutenticacionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Taller_Industrial_Brenes_Web.Controllers
+{
+    public class AutenticacionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
