0d3c34bd1867780b0a72e48605400b34a866c17b
esveny <bbrenes60291@ufide.ac.cr>
2025-06-07 14:56:49 -0600

    a

--- Taller Industrial Brenes API/obj/Debug/net8.0/Taller Industrial Brenes API.AssemblyInfo.cs
+++ Taller Industrial Brenes API/obj/Debug/net8.0/Taller Industrial Brenes API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Taller Industrial Brenes API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58c091f3f7d9ff5a7f02fbee8881b2e38f8960c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7a53a510bdf9e5295ab67a4cb9c9fb52676509e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Taller Industrial Brenes API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Taller Industrial Brenes API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
