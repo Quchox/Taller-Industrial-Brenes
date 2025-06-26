dc432fe8c9602504ee5e019ca0a3a51411610514
Paola Segura <145728086+Paobella@users.noreply.github.com>
2025-06-26 16:25:05 -0600

    vistas clientes

---
+++ Taller Industrial Brenes Web/Controllers/Home/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Taller_Industrial_Brenes_Web.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace Taller_Industrial_Brenes_Web.Controllers.Home
+{
+    public class HomeController : Controller
+    {
+        private readonly IHttpClientFactory _httpClient;
+        private readonly IConfiguration _config;
+
+        public HomeController(IHttpClientFactory httpClient, IConfiguration config)
+        {
+            _httpClient = httpClient;
+            _config = config;
+        }
+            public IActionResult Index()
+            {
+                return View();
+            }
+        
+    }
+}
